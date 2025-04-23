16ec4dd64b7fb0349aa6d65e333e22ecee68db1c
moremedina <moremedivale@gmail.com>
2025-04-23 09:18:14 -0300

    Add files via upload

---
+++ tk More Medina.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+def main():
+    # Crear la ventana principal
+    ventana = tk.Tk()
+    ventana.title("Ejercicio 1 - More Medina")
+    ventana.geometry("500x400")
+    ventana.configure(bg="#641c34")
+    ventana.mainloop()
+
+
+if __name__=="__main__":
+    main()
